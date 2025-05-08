ea79fbe977cc0a8a0c8636a180eb667aefb03fc7
Emilio Castro Lagunas <EmilioCLDK@gmail.com>
2025-05-08 14:26:39 +0200

    fixed environment by downgrading python to 3.10

--- tasks.py
+++ tasks.py
@@ -1,6 +1,21 @@
 from robocorp.tasks import task
 
+import requests
+
+def fetch_pokemon():
+    url = "https://pokeapi.co/api/v2/pokemon/ditto"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(f"Name: {data['name']}")
+        print(f"ID: {data['id']}")
+        print(f"Height: {data['height']}")
+        print(f"Weight: {data['weight']}")
+    else:
+        print("Failed to fetch Pokémon data")
+
+# The main function Robocorp uses as the default task:
 @task
-def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+def main():
+    fetch_pokemon()
